b29a80db93215f04c0c296de483e8d27a967790e
Necromoshka <sergeyklimets@gmail.com>
2016-11-16 17:03:17 +0300

    add ru language

--- journal/userMain/models.py
+++ journal/userMain/models.py
@@ -3,5 +3,8 @@ from django.db import models
 # Create your models here.
 
 class userMainTeble(models.Model):
-    fild_one=models.CharField(max_length=200)
+    fild_one=models.CharField(max_length=200, verbose_name='Текстовое поле')
     pub_date=models.DateTimeField('Дата публикации')
+    class Meta:
+        verbose_name="Тестовая запись"
+        verbose_name_plural = "Тестовые записи"
