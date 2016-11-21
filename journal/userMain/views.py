8a1920ea4e64ab8ed80a02ee8a8d5cf1a6e0769b
Necromoshka <sergeyklimets@gmail.com>
2016-11-21 17:16:26 +0300

    insert pagination

--- journal/userMain/views.py
+++ journal/userMain/views.py
@@ -2,11 +2,22 @@ from django.shortcuts import render
 #from django.http import Http404
 #from django.http import HttpResponse
 # Create your views here.
-
+from django.core.paginator import Paginator, InvalidPage
 from .models import userMainTeble
 #from .forms import
 
+
+
 def index(request):
-    rows=userMainTeble.objects.order_by('id')
-    context={'rows':rows}
+    try:
+        page_num = request.GET["page"]
+    except KeyError:
+        page_num = 1
+    rows = userMainTeble.objects.order_by('id')
+    paginator = Paginator(userMainTeble.objects.all(), 10)
+    try:
+        pg = paginator.page(page_num)
+    except InvalidPage:
+        pg = paginator.page(1)
+    context = {'rows': rows, "pg": pg}
     return render(request,'userMain/index.html', context)
